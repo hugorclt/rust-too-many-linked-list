a293d6a94cae8fd5368511b1b5eda56d6efb41d7
Hugo Récolet <hugorecolet@gmail.com>
2023-01-22 22:52:02 +0100

    [init]

---
+++ src/first.rs
@@ -0,0 +1,23 @@
+pub struct List {
+	Head : Link,
+}
+
+enum Link {
+	Empty,
+	More(Box<Node>),
+}
+
+struct Node {
+	elem : i32,
+	next : Link,
+}
+
+impl List {
+	pub fn new() -> Self {
+		List { Head: Link::Empty };
+	}
+
+	pub fn push(&mut self, elem : i32 ) -> void {
+
+	}
+}
